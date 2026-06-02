b92cfcdfa08e57781209f4916d7e919871689c6f
23f2004290 <manyakhare30@gmail.com>
2026-06-02 21:12:04 +0530

    Update CORS middleware configuration

--- api/index.py
+++ api/index.py
@@ -6,10 +6,11 @@ import numpy as np
 app=FastAPI()
 
 app.add_middleware(
-CORSMiddleware,
-allow_origins=["*"],
-allow_methods=["POST"],
-allow_headers=["*"]
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 with open("q-vercel-latency.json") as f:
